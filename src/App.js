b3798945bdcafa73f70113276b0fa18bf9c2f1a6
juanes <juanesc36936969@gmail.com>
2024-06-26 14:38:43 -0500

    proyecto-finalizado

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ItemDetailContainer from './components/itemDetailContainer/ItemDetailCont
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Cart from './components/cart/Cart'
+import Checkout from './components/checkout/Checkout';
 function App() {
   return (
     <CartProvider>
@@ -18,6 +19,7 @@ function App() {
       <Route path='/categories/:categoryId' element={<ItemListContainer greeting='Categoria'/> }/>
       <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
       <Route path='/cart' element={<Cart/>}/>
+      <Route path='/checkout' element={<Checkout/>}/>
     </Routes>
     </BrowserRouter> 
     </CartProvider>
